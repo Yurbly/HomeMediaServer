ced8ff19480106a1d30b938999e85216546a5de9
Iurii Novikov <yunov1@gmail.com>
2019-02-09 18:27:22 +0300

    url is set

--- back/index.js
+++ back/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const https = require('https');
 
+
+const hostname = "192.168.1.2";
+// const port = 3000;
+
+// const server = https.createServer(app).listen();
 app.get("/", (req, res) => {
    res.send("Привеееееет!");
 });
 
-app.listen(3000, () => {
+app.listen(3000, hostname, () => {
     console.log("Home media server listen on port 3000!");
 });
